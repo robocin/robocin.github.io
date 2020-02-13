a12ca4df38d68b7b0f812521e7189987d9e29cce
Roberto <rcf6@cin.ufpe.br>
2020-02-13 15:29:17 -0300

    Adding social networks on members page

--- js/membros.js
+++ js/membros.js
@@ -24,26 +24,10 @@ readTextFile("https://robocin.com.br/json/membros.json", function(text)
     $(document).ready(function(){
         var i = 0;
 
-        /*
-        member_info_list = document.createElement('ul');
-        $(member_info_list).addClass('social');
+        var dados = JSON.parse(text);
 
-        $(member_info_list).append($('<li>').append($('<a>').attr('href', '#')
-                                                    .append($('<span>').addClass('icon-facebook'))));
-
-        $(member_info_list).append($('<li>').append($('<a>').attr('href', '#')
-                                                    .append($('<span>')).addClass('icon-twitter')));
-        
-        $(member_info_list).append($('<li>').append($('<a>').attr('href', '#')
-                                                    .append($('<span>').addClass('icon-linkedin'))));
-
-        $(member_info_list).append($('<li>').append($('<a>').attr('href', '#')
-                                                    .append($('<span>').addClass('icon-instagram'))));
-        */
-
-       var dados = JSON.parse(text);
-
-       for(i = 0; i < dados.length; i++){
+        for(i = 0; i < dados.length; i++){
+            var social_network = false;
             member_info = document.createElement('div');
             $(member_info).addClass("col-md-6 col-lg-3 mb-4 aos-init aos-animate");
             $(member_info).attr("data-aos","fade-up");
@@ -63,16 +47,49 @@ readTextFile("https://robocin.com.br/json/membros.json", function(text)
             $(member_name).addClass('p-3');
             $(member_name).append($("<h3>"+dados[i].nome+"</h3>"));
             $(member_name).append($("<span><span>").addClass("position").attr('tkey',dados[i].categoria));
-            console.log(dados[i].categoria)
 
+            member_info_list = document.createElement('ul');
+            $(member_info_list).addClass('social');
 
-            // $(member_info_list).appendTo(member_figure_info);
+            if (dados[i].facebook) {
+                $(member_info_list).append($('<li>').append($('<a>').attr('href', dados[i]['facebook'])
+                                    .attr("target", "_blank").append($('<span>').addClass('icon-facebook'))));
+                social_network = true;
+            }
+    
+            if (dados[i].twitter) {
+                $(member_info_list).append($('<li>').append($('<a>').attr('href', dados[i]['twitter'])
+                                    .attr("target", "_blank").append($('<span>')).addClass('icon-twitter')));
+                social_network = true;
+            }
+            
+            if (dados[i].linkedin) {
+                $(member_info_list).append($('<li>').append($('<a>').attr('href', dados[i]['linkedin'])
+                                    .attr("target", "_blank").append($('<span>').addClass('icon-linkedin'))));
+                social_network = true;
+            }
+    
+            if (dados[i].instagram) {
+                $(member_info_list).append($('<li>').append($('<a>').attr('href', dados[i]['instagram'])
+                                    .attr("target", "_blank").append($('<span>').addClass('icon-instagram'))));
+                social_network = true;
+            }
+
+            if (dados[i].github) {
+                $(member_info_list).append($('<li>').append($('<a>').attr('href', dados[i]['github'])
+                                    .attr("target", "_blank").append($('<span>').addClass('icon-github'))));
+                social_network = true;
+            }
+
+            if (social_network) {
+                $(member_info_list).appendTo(member_figure_info);
+            }
             $(member_img).appendTo(member_figure_info);
             $(member_figure_info).appendTo(member_info_class);
             $(member_name).appendTo(member_info_class);
             $(member_info_class).appendTo(member_info);
             $(member_info).appendTo('#team-member-images');
-       }
+        }
 
         let cookieLang = localStorage.getItem('lang')
         langCode = cookieLang
